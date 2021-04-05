50f9cce0e33169a1ec3f732cf62e8d2c3672a06c
Luke Billings <lukecbillings@gmail.com>
2021-04-05 11:23:32 +0100

    setup complete

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" height="100" />
+    <img alt="Vue logo" src="./assets/logo.png" height="100">
     <h1>Task Manager</h1>
-    <p>Let's build our first Vue.js app!</p>
+    <TasksList />
   </div>
 </template>
 
 <script>
-export default {
-  name: "App",
-};
-</script>
+import TasksList from './components/TasksList.vue'
 
-<style lang="scss">
-@import "./assets/style.scss";
-</style>
+export default {
+  name: 'App',
+  components: {
+    TasksList
+  }
+}
+</script>
---
+++ src/components/TasksList.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="tasks">
+    <div class="task-card"></div>
+    <h3>Title of a task</h3>
+    <p>Desciption of task</p>
+  </div>
+</template>
